2b3779906939e42ce347f13c3024776e2c36a3f5
Anilmemis <64617703+Anilmemis@users.noreply.github.com>
2023-05-11 21:31:52 +0300

    Add files via upload
    Basit şekilde yazılmış port scanner. 
    Hedef ip adresini ve port aralığını değiştirip kullanabilirsiniz.
    ------------------------------------------------------------------
    Simply written port scanner.
    You can change the target ip address and port range and use it.

---
+++ port_service_Scan.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import socket
+import os
+import sys
+
+def retBanner(ip,port):
+        try:
+            socket.setdefaulttimeout(2)
+            s = socket.socket()
+            s.connect((ip,port))
+            banner = s.recv(1024)
+            return banner
+        except:
+            pass
+            
+
+def checkVulns(banner,filename):
+    f = open(filename,"r+")
+    for line in f.readlines():
+        if line.strip("\n") in banner:
+            print("[+] Server is Vulnerable :" + banner.strip("\n"))
+
+def main():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+        if not os.path.isfile(filename):
+            print("[-] File Doesnt Exist!")
+            exit(0)
+        if not os.access(filename,os.R_OK):
+            print("[-] Access Denied!")
+            exit(0)    
+    else:
+        print("[-] Usage: " + str(sys.argv[0]) + "<vuln filename>")
+        exit(0)
+    portlist = range(1,63000)
+    for x in range(135,139):
+        ip = "1.1.1."+str(x) #enter the target ip adr or domain and up to range in change limitted
+        for port in portlist:
+            banner = retBanner(ip,port)
+            if banner:
+                print ("[+]"+ ip + "/" + str(port) + " + " + str(banner))
+                checkVulns(banner,filename)
+                
+main()
